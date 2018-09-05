6316ddfa0c4018827019fe72608a1108637d4ddb
ripo <ripohassan5@gmail.com>
2018-09-05 18:13:47 +0600

    init commit

---
+++ app/src/main/java/com/example/ripo/websiteload/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.ripo.websiteload;
+
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+  private SwipeRefreshLayout refreshLayout;
+  private WebView htmlWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        refreshLayout = findViewById(R.id.swipeLayout);
+        refreshLayout.setOnRefreshListener(this);
+        htmlWebView = (WebView)findViewById(R.id.webView);
+
+        htmlWebView.setWebViewClient(new CustomWebViewClient());
+        WebSettings webSetting = htmlWebView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setDisplayZoomControls(true);
+        htmlWebView.loadUrl("https://www.google.com");
+    }
+
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                refreshLayout.setRefreshing(false);
+            }
+
+        }, 3000);
+
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
+
+}
